e54a518aca9cc042208b995503f40037726539ad
Ryan Chen <ryantoffeechen@gmail.com>
2019-11-12 09:53:32 -0800

    added background image

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import map from './map.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+        <ImageBackground source={map} style={{width: '100%', height: '100%'}}>
+          
+        </ImageBackground>
     </View>
   );
 }
