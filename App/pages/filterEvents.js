81cdb9e51a469cf43840aba149ce5941958f2447
gabbydr <gabbyr@stanford.edu>
2019-11-15 00:11:11 -0800

    added multiple pins to filterEvents

--- App/pages/filterEvents.js
+++ App/pages/filterEvents.js
@@ -11,6 +11,13 @@ export default class FilterEvents extends React.Component {
     headerTitle: (<Image source={Images.logo}/>),
   };
 
+  state = {
+    coordinates : [
+      { name: '1', latitude: 37.427799, longitude: -122.171198},
+      { name: '2', latitude: 37.425682, longitude: -122.167445},
+    ]
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -30,7 +37,18 @@ export default class FilterEvents extends React.Component {
             }}
             title="HELP ME">
         </Marker>
-        
+
+        {this.state.coordinates.map(marker => (
+          <Marker
+          key={marker.name}
+          coordinate={
+            {latitude: marker.latitude,
+            longitude: marker.longitude}}
+          title={marker.name}
+          >
+          </Marker>
+        ))
+      }
         </MapView>
       </View>
     );
