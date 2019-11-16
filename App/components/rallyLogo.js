b2dfc633c05c5c66cb6b044a60ec923672a05332
gabbydr <gabbyr@stanford.edu>
2019-11-15 23:54:50 -0800

    changed rallyLogo styles

--- App/components/rallyLogo.js
+++ App/components/rallyLogo.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, Image, TouchableOpacity } from 'react-native'
+import { StyleSheet, View, Image, TouchableOpacity, Dimensions } from 'react-native'
 import { Images, Metrics } from '../Themes';
 
 export default class RallyLogo extends Component {
-  
+
   render () {
     return (
       <View style={styles.rallyLogo}>
         <TouchableOpacity
           onPress={() => this.props.navigation.navigate('Home')}
         >
-          <Image source={Images.logo}/>
+          <Image source={Images.rally}/>
         </TouchableOpacity>
       </View>
     );
   }
 }
+const { height, width } = Dimensions.get('window')
 
 const styles = StyleSheet.create({
   rallyLogo: {
+    height: height * .2,
+    width: width,
     position: 'absolute',
     top: '5%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
   },
 });
