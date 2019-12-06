afab5e16b238d4d5b8858b087a85b863df1ee4f9
abromanos <aromanos@stanford.edu>
2019-12-05 22:01:15 -0800

    login

--- App/pages/LoginScreen.js
+++ App/pages/LoginScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, SafeAreaView, Image, TextInput, Button, Alert } from 'react-native';
+import { StyleSheet, SafeAreaView, Image, TextInput, Button, Alert} from 'react-native';
 import firestore from '../../firebase';
 import firebase from 'firebase';
 import { Images } from '../Themes';
